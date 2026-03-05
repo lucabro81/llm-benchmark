5a546a516e32d36f59fb22dab1ac2d9431ea062d
lucabro81 <luca.brognara81@gmail.com>
2026-03-05 12:41:46 +0100

    feature: new fixture with turborepo, nuxt and a minimal package of ui components

--- fixtures/creation/veevalidate-zod-form-nuxt-rag/target_project/packages/elements/src/lib/utils.ts
+++ fixtures/creation/veevalidate-zod-form-nuxt-rag/target_project/packages/elements/src/lib/utils.ts
@@ -2,8 +2,6 @@ import { type ClassValue, clsx } from 'clsx'
 import { ref, watch, type Ref } from "vue";
 import { twMerge } from 'tailwind-merge'
 import type { FormContext } from 'vee-validate';
-import { z } from "zod";
-import type { $ZodType, $ZodTypeInternals } from 'zod/v4/core';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -32,8 +30,4 @@ export function valueUpdater<T>(updaterOrValue: T, ref: Ref) {
 
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export function delay<T extends Readonly<{ [k: string]: $ZodType<unknown, unknown, $ZodTypeInternals<unknown, unknown>>; }>>(obj: z.ZodObject<T>, ms = 0) {
-  return z.preprocess(async (val) => { await sleep(ms); return val }, obj);
 }
